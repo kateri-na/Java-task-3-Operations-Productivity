ba7775b31fc3ee08eb29059a5c6088ddc76a81bb
katerina <katerina_panina1@mail.ru>
2024-04-09 18:19:29 +0300

    test remove method for both containers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,9 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<Integer>(List.of(56, 48, 33));
         LinkedList<Integer> linkedList = new LinkedList<Integer>(List.of(56, 48,33));
+
         long startTime, endTime;
+
         System.out.println("Время выполнения операции добавления (в наносекундах):");
         startTime = System.nanoTime();
         arrayList.add(88);
@@ -18,5 +20,14 @@ public class Main {
         endTime = System.nanoTime();
         System.out.printf("Linked List: %d \n", endTime - startTime);
 
+        System.out.println("Время выполнения операции удаления (в наносекундах):");
+        startTime = System.nanoTime();
+        arrayList.remove(3);
+        endTime = System.nanoTime();
+        System.out.printf("Array List: %d \n", endTime - startTime);
+        startTime = System.nanoTime();
+        linkedList.remove(3);
+        endTime = System.nanoTime();
+        System.out.printf("Linked List: %d \n", endTime - startTime);
     }
 }
