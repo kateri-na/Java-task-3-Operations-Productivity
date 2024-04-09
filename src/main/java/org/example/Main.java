272db1d0d3c71d8e596c01069b79e6b3f024cec3
katerina <katerina_panina1@mail.ru>
2024-04-09 18:12:21 +0300

    test add method for both containers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,22 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ArrayList<Integer> arrayList = new ArrayList<Integer>(List.of(56, 48, 33));
+        LinkedList<Integer> linkedList = new LinkedList<Integer>(List.of(56, 48,33));
+        long startTime, endTime;
+        System.out.println("Время выполнения операции добавления (в наносекундах):");
+        startTime = System.nanoTime();
+        arrayList.add(88);
+        endTime = System.nanoTime();
+        System.out.printf("Array List: %d \n", endTime - startTime);
+        startTime = System.nanoTime();
+        linkedList.add(88);
+        endTime = System.nanoTime();
+        System.out.printf("Linked List: %d \n", endTime - startTime);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
